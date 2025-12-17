e0fcdb191d44b50aaff715e3ebb8469d3a9b6e09
Saijanumuri <srkbvm2002@gmail.com>
2025-12-17 22:12:35 +0530

    added regions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-
+import React,{useState} from "react";
 
 function App() {
+  const [eu,setEu]=useState("");
   return (
     <div>
+      <label for="region">Region: </label>
+      <select id="region" onChange={(e)=>setEu(e.target.value)} >
+        <option value="">select region</option>
+        <option value="EU">EU</option>
+        <option value="NA">NA</option>
+        <option value="FE">FE</option>
+      </select>
+      <br/>
+      <label for="country">Country: </label>
+      {eu=="EU"&&
+      <select id="country">
+        <option>AE</option>
+        <option>AT</option>
+        <option>LU</option>
+        <option>LV</option>
+      </select>
+      }
       <label for="FeedID">FeedID: </label>
       <input className="FeedID" id="FeedID" name="FeedID" /><br/><br/>
       <label for="cd">CD: </label>
@@ -13,6 +31,7 @@ function App() {
       <input className="sp" id="sp" name="sp" /><br/><br/>
       <label for="fp">CD: </label>
       <input className="fp" id="fp" name="fp" />
+      
       
     </div>
 
